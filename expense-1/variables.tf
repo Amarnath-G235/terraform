4b128847fb56dd95ffe9d7d6f271d2528f3f0efd
Amarnathg <amarnath.gotru4@gmail.com>
2024-09-27 16:30:02 +0530

    terraform

---
+++ expense-1/variables.tf
@@ -0,0 +1,13 @@
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql","backend","frontend"]
+    description = "description"
+}
+
+variable "domain_name" {
+    default = "ukom81s.online"
+}
+
+variable "zone_id" {
+    default = "Z1011040F14N5MVPIIUV"
+}
