1703a851d4dd947309017795cb2495db98b15246
Amarnathg <amarnath.gotru4@gmail.com>
2024-11-19 12:06:50 +0530

    terraform

--- ecc2/ec2.tf
+++ ecc2/ec2.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "allow_ssh_terraform" {
     }
 }
 
-resource "aws_ec2_instance" "terraform" {
+resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.ids]
