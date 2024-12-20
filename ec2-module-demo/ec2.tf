980d4db13da4e45e1fba05710abb59e19bcaa8d6
Amarnathg <amarnath.gotru4@gmail.com>
2024-12-20 12:13:44 +0530

    terraform

--- ec2-module-demo/ec2.tf
+++ ec2-module-demo/ec2.tf
@@ -1,6 +1,6 @@
 module "ec2" {
     source = "../terraform-aws-ec2"
     ami_id = var.ami_something
-    instance_type = "t3.small"
+    instance_type = "t3.medium"
     security_group_ids = ["sg-0de47c9501265bafc"]
 }
